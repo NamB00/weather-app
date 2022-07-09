7b7d647b409977eacc487cf3241154c4600f23ff
Bui Nhut Nam <nhutnamtube@gmail.com>
2022-07-09 20:01:14 +0700

    key 2 fix4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,12 +5,11 @@ import { environment as common } from './environment';
 
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: '...',
-    authDomain: 'project.firebaseapp.com',
-    databaseURL: 'https://project.firebaseio.com',
-    projectId: 'project',
-    storageBucket: 'project.appspot.com',
-    messagingSenderId: '...',
-  },
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostLabel: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'cca929f7cdmshd1be5a472559672p14237fjsn5c767b978ab9',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,21 @@ export const environment = {
   XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
 
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: 'cca929f7cdmshd1be5a472559672p14237fjsn5c767b978ab9'
+  XRapidAPIKeyHeaderValue: 'cca929f7cdmshd1be5a472559672p14237fjsn5c767b978ab9',
+
 };
+
+
+// export const environment = {
+//   production: false,
+//   weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+//   XRapidAPIHostLabel: 'X-RapidAPI-Host',
+//   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+//   XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+
+//   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+//   XRapidAPIKeyHeaderValue: 'cca929f7cdmshd1be5a472559672p14237fjsn5c767b978ab9'
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
